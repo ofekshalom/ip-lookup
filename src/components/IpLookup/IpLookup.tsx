bdadb04e64ac12ab103e69ed37428ea2272b14dc
ofekshalom <ofekshalom123@gmail.com>
2025-02-10 17:18:24 +0200

    set up create react up with mui

---
+++ src/components/IpLookup/IpLookup.tsx
@@ -0,0 +1,5 @@
+import { TextField } from "@mui/material";
+
+export const IpLookup = () => {
+  return <TextField />;
+};
---
+++ src/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import reportWebVitals from "./reportWebVitals";
+import { IpLookup } from "./components/IpLookup/IpLookup";
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <IpLookup />
+  </React.StrictMode>
+);
+
+reportWebVitals();
